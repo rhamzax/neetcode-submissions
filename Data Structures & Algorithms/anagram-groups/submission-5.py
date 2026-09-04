bab00479380373960df4242ebd3ba8dd3fe861d0
Hamza Amar <75512875+rhamzax@users.noreply.github.com>
2026-09-04 12:29:56 -0600

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #Hashmap to save and use the characters as the keys
+        grouping = {}
+        for s in strs:
+            numChar = [0] * 26
+            for char in s:
+                index = ord(char) - ord("a")
+                numChar[index] += 1
+            numChar = tuple(numChar)
+            if numChar in grouping:
+                listofWords = grouping[numChar]
+                listofWords.append(s)
+                grouping[numChar] = listofWords
+                #grouping[numChar] = grouping[numChar].append(s)
+            else:
+                grouping[numChar] = [s]
+        return list(grouping.values())  
